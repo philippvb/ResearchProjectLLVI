9ad3282eb48619c20aaac480b40f9aae95ddc9a7
Philipp von Bachmann <philippvonbachmann@web.de>
2022-01-04 11:12:22 +0100

    Updated MNIST base experiment

--- BasicExample/experiments/Classification/MNIST/LLVI_MNIST.py
+++ BasicExample/experiments/Classification/MNIST/LLVI_MNIST.py
@@ -1,13 +1,19 @@
 import sys
-sys.path.append('/Users/philippvonbachmann/Documents/University/ResearchProject/ResearchProjectLLVI')
+sys.path.append('/Users/philippvonbachmann/Documents/University/WiSe2122/ResearchProject/ResearchProjectLLVI/BasicExample')
 import torch
 import torchvision
 
+from src.network.feature_extractor import CNN
+from src.network.Classification import LLVIClassification
+from src.network import LikApprox, PredictApprox
+from src.weight_distribution.Full import FullCovariance
+from src.network import LLVINetwork
+
 batch_size_train = 32
 batch_size_test = 1000
-filepath = "/Users/philippvonbachmann/Documents/University/ResearchProject/ResearchProjectLLVI/BasicExample/files"
+filepath = "/Users/philippvonbachmann/Documents/University/WiSe2122/ResearchProject/ResearchProjectLLVI/BasicExample/datasets/Classification"
 train_loader = torch.utils.data.DataLoader(
-  torchvision.datasets.MNIST(filepath, train=True, download=True,
+  torchvision.datasets.MNIST(filepath, train=True, download=False,
                             transform=torchvision.transforms.Compose([
                               torchvision.transforms.ToTensor(),
                               torchvision.transforms.Normalize(
@@ -17,7 +23,7 @@ train_loader = torch.utils.data.DataLoader(
 n_datapoints = batch_size_train * len(train_loader)
 
 test_loader = torch.utils.data.DataLoader(
-torchvision.datasets.MNIST(filepath, train=False, download=True,
+torchvision.datasets.MNIST(filepath, train=False, download=False,
                             transform=torchvision.transforms.Compose([
                             torchvision.transforms.ToTensor(),
                             torchvision.transforms.Normalize(
@@ -34,43 +40,32 @@ torchvision.datasets.MNIST(filepath, train=False, download=True,
                             ])),
 batch_size=batch_size_test, shuffle=True)
 
-def test_LLVI(network):
-  network.train_model(train_loader=train_loader, n_datapoints=n_datapoints, epochs=10, samples=2)
-  network.test(test_loader=test_loader)
-  network.test_confidence(test_loader=test_loader, ood_test_loader=ood_test_loader)
+def test_confidence(model:LLVINetwork, test_loader, ood_test_loader=None, samples=5):
+    model.eval()
+    confidence_batch = []
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data, method=PredictApprox.MONTECARLO ,samples=samples)
+            pred, _ = torch.max(output, dim=1) # confidence in choice
+            confidence_batch.append(torch.mean(pred))
+        print(f"The mean confidence for in distribution data is: {sum(confidence_batch)/len(confidence_batch)}")
+
+    ood_confidence_batch = []
+    with torch.no_grad():
+        for data, target in ood_test_loader:
+            output = model(data, method=PredictApprox.MONTECARLO ,samples=samples)
+            pred, _ = torch.max(output, dim=1) # confidence in choice
+            ood_confidence_batch.append(torch.mean(pred))
+        print(f"The mean confidence for out-of distribution data is: {sum(ood_confidence_batch)/len(ood_confidence_batch)}")
 
 
-from BasicExample.src.LLVI_network import LLVI_network_diagonal, LLVI_network_KFac, Log_likelihood_type
-from BasicExample.src.basic_CNN import Net, NonBayesianNet
-# test the baseline net
-# non_bayes_net = NonBayesianNet()
-# non_bayes_net.train_model(train_loader, epochs=10)
-# non_bayes_net.test(test_loader)
-# non_bayes_net.test_confidence(test_loader, ood_test_loader)
+lr = 1e-4
+feature_extractor = CNN(optimizer=torch.optim.Adam, lr=lr, weight_decay=5e-4)
+dist = FullCovariance(50, 10, lr=lr)
+net = LLVIClassification(50, 10, feature_extractor, dist,
+prior_log_var=-1, optimizer_type=torch.optim.Adam,
+tau=1, lr=lr)
 
-
-# # test the diagonal gaussian net
-feature_extractor_net = Net()
-Diag_net = LLVI_network_diagonal(feature_extractor=feature_extractor_net,
-feature_dim=50, out_dim= 10,
-prior_mu=0, prior_log_var=-2,
-init_ll_mu=0,init_ll_log_var=0,
-lr=1e-2, tau=10, bias=False, loss=Log_likelihood_type.CATEGORICAL)
-
-# Diag_net.train_without_VI(train_loader, epochs=10)
-# test_LLVI(Diag_net)
-for data, target in test_loader:
-  result = Diag_net.predict(data)
-  print(result.sum(-1))
-
-# test KFac net
-# feature_extractor_net = Net()
-# KFac_net = LLVI_network_KFac(feature_extractor=feature_extractor_net,
-# feature_dim=50, out_dim= 10,
-# A_dim=50, B_dim=10,
-# prior_mu=0, prior_log_var=-2,
-# init_ll_mu=0, init_ll_cov_scaling=0.1,
-# lr=1e-2, tau=1)
-# test_LLVI(KFac_net)
-
-
+# net.train_without_VI(train_loader, epochs=2)
+net.train_model(train_loader, epochs=1, samples=10, n_datapoints=n_datapoints)
+test_confidence(net, test_loader, ood_test_loader, samples=10)
